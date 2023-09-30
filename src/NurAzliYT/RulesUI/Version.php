90cdb6f46b07b2511cd4654652a4f7fb1d9f1e64
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2023-09-30 12:46:12 +0700

    Create Version.php

---
+++ src/NurAzliYT/RulesUI/Version.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace NurAzliYT\RulesUI;
+
+use NurAzliYT\RulesUI\Main;
+use pocketmine\version\version;
+
+class Version extends Version {
+  public $version;
+  public Main $main;
+
+  # New Version
+  public function Version {
+  $version->newversion(0.0.1);
+}
+    }
